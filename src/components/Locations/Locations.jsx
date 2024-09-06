67e1766779a3d93c9095d914a977c6ff3e1ffbcc
SanjarbekH02 <sanjarbekkhakimov21409@gmail.com>
2024-09-06 18:43:13 +0500

    task complated

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -2,43 +2,30 @@ import React, { useEffect, useState } from 'react';
 import './Locations.css'
 
 const Location = () => {
-    const [locations, setLocations] = useState([])
-
-    useEffect(() => {
-        fetch("https://autoapi.dezinfeksiyatashkent.uz/api/locations")
-            .then((res) => {
-                return res.json()
-            })
-            .then((item) => {
-                setLocations(item?.data)
-            })
-    }, [])
     return (
         <div className='bg-slate-900'>
             <div className="container mx-auto flex">
                 <div className="">
                     <h1 className="text-4xl text-white">LOCATION</h1>
                     <ul className="list-none flex-col">
-                        {
-                            locations?.map((elem, i) => (
-                                <li className='pt-3'>
-                                    <a href="#" className="text-slate-500 text-xl underline">{elem?.name}</a>
-                                </li>
-                            ))
-                        }
+                        <li className='pt-3'>
+                            <a href="#" className="text-slate-500 text-xl underline">Dubai Mall</a>
+                        </li>
+                        <li className='pt-3'>
+                            <a href="#" className="text-slate-500 text-xl underline">Sharja Airport</a>
+                        </li>
                     </ul>
                 </div>
 
                 <div className="ms-40 city-block    ">
                     <h1 className="text-4xl text-white">CITY</h1>
                     <ul className="list-none flex-col">
-                        {
-                            locations?.map((elem, i) => (
-                                <li className='pt-3'>
-                                    <a href="#" className="text-slate-500 text-xl underline">{elem?.text}</a>
-                                </li>
-                            ))
-                        }
+                        <li className='pt-3'>
+                            <a href="#" className="text-slate-500 text-xl underline">Dubai</a>
+                        </li>
+                        <li className='pt-3'>
+                            <a href="#" className="text-slate-500 text-xl underline">Sharja</a>
+                        </li>
                     </ul>
                 </div>
 
